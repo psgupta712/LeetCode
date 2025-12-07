4c11b9c5b734b03c5639de92c81463b2c33f0c77
Priyanshu Gupta <118977399+psgupta712@users.noreply.github.com>
2025-12-07 12:44:02 +0530

    Time: 0 ms (100%), Space: 10 MB (62.68%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -11,17 +11,16 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        int count = 0;
-        ListNode* temp = head;
-        while(temp != NULL){
-            count++;
-            temp=temp->next;
+        ListNode *slow = head, *fast = head;
+
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        count /= 2;
-        temp = head;
-        while(count--){
-            temp=temp->next;
-        }
-        return temp;
+        return slow;
     }
 };
+
+    //with the help of sloe and fast pointer 
+    //slow pointer takes 1 step
+    //fast pointer takes 2 step
