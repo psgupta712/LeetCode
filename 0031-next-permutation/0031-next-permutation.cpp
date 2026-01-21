cb37e389c649fbf3e00780d0b9fdafffdae41acd
PRIYANSHU GUPTA <118977399+psgupta712@users.noreply.github.com>
2026-01-21 21:51:10 +0530

    Time: 0 ms (100%), Space: 15.8 MB (32.02%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        //firstly we have to find that point at which we can increase lexicographical order.
+        int idx = -1;
+        int n = nums.size();
+        for(int i=n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                idx = i;
+                break;
+            }
+        }
+
+        //if breaking point does not found then reverse the array
+        if(idx == -1){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+
+        // find the next greater element of nums[idx], then swap
+        for(int i=n-1; i>idx; i--){
+            if(nums[i] > nums[idx]){
+                swap(nums[idx], nums[i]);
+                break;
+            }
+        }
+
+
+        // After swapping sort the array from the idx+1 , nums.end()
+        reverse(nums.begin() + idx + 1, nums.end());
+        return ;
     }
 };
