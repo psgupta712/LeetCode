34c265668a5541997029b67d93d2604cc70aaf43
PRIYANSHU GUPTA <118977399+psgupta712@users.noreply.github.com>
2026-01-25 18:42:25 +0530

    Time: 3 ms (89.36%), Space: 23.8 MB (58.92%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,15 +4,28 @@ public:
         
         sort(intervals.begin(), intervals.end());
 
-        vector<vector<int>>ans;
-        ans.push_back(intervals[0]);
+        // vector<vector<int>>ans;
+        // ans.push_back(intervals[0]);
 
-        for(int i=1; i<intervals.size(); i++){
-            if(ans.back()[1] >= intervals[i][0]){
-                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
-            }
-            else {
+        // for(int i=1; i<intervals.size(); i++){
+        //     if(ans.back()[1] >= intervals[i][0]){
+        //         ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+        //     }
+        //     else {
+        //         ans.push_back(intervals[i]);
+        //     }
+        // }
+        // return ans;
+
+        
+        vector<vector<int>>ans;
+
+        for(int i=0; i<intervals.size(); i++){
+            if(ans.empty() || intervals[i][0] > ans.back()[1]){
                 ans.push_back(intervals[i]);
+            }
+            else{
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
         }
         return ans;
